61649db3299ab1356fea8094ec3f106db826ea52
laowang <415813765@qq.com>
2019-09-11 20:19:16 +0800

    'init'

---
+++ test/hello.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 老王专用
+ * Date: 2019/9/11
+ * Time: 19:44
+ */
+
+require "../vendor/autoload.php";
+
+use laowang\UploadFile;
+
+$upload = new UploadFile();
